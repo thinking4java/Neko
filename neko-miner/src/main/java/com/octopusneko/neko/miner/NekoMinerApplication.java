5310ed03934bbfdd7f55878fa40d08f39316fb4b
octopusneko <thinking4java@gmail.com>
2021-07-20 14:15:26 +0800

    Init sub project neko-miner

---
+++ neko-miner/src/main/java/com/octopusneko/neko/miner/NekoMinerApplication.java
@@ -0,0 +1,13 @@
+package com.octopusneko.neko.miner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekoMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekoMinerApplication.class, args);
+	}
+
+}
---
+++ neko-miner/src/test/java/com/octopusneko/neko/miner/NekoMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octopusneko.neko.miner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekoMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
